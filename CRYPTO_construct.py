46ff643cd7f172679ef1d9695b85c33d760158b1
the-salty-austin <68510743+the-salty-austin@users.noreply.github.com>
2021-07-17 00:34:09 +0800

    Delete CRYPTO_construct.py

--- CRYPTO_construct.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('Database\\test.sqlite')
-cur = conn.cursor()
-
-cur.execute('CREATE TABLE IF NOT EXISTS Record (close REAL, high REAL, low REAL, vol REAL, time INTEGER)')
