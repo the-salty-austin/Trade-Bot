4d5636a58e7ef2b6d418447555d8d7f77545792f
the-salty-austin <68510743+the-salty-austin@users.noreply.github.com>
2021-07-17 00:33:01 +0800

    Update CRYPTO_config.py

--- CRYPTO_config.py
+++ CRYPTO_config.py
@@ -1,2 +1,2 @@
-apiKey = 'aUrxRDO1w3WYultNlmY9tfTK5dfNzquYrOo85hJUW7Zq7esU00fzIAOFmgstbp21'
-apiSecurity = '3gXhalURL0smamn0M1D1Sdm5SoWRFN6yBTOliSaSQyMdQMSvaD9QRUFoSbkey8SR'
+apiKey = '### censored for privacy reasons ###'
+apiSecurity = '### censored for privacy reasons ###'
